f68718e31c5b34a7931afc49f495cea50cf6dcf7
RealStack-Apps <76439804+ali-stacked@users.noreply.github.com>
2021-03-02 09:12:47 -0500

    added folder structure

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-var express = require('express');
-var port = process.env.PORT || 80;
-var app = express(),
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Configuring the database
+const dbConfig = require('./config/database.config.js');
+const mongoose = require('mongoose');
+const port = process.env.PORT || 80;
+
+const app = express(),
 path = require('path'),
 publicDir = path.join(__dirname,'public');
-
 app.use(express.static(publicDir))
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
+
+// parse application/json
+app.use(bodyParser.json())
 
 app.listen(port);
 module.exports = app;
